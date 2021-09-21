bf8e4291e58d98fa5eb7d64560d9706b0be8bf3a
Liss-veta <90349819+Liss-veta@users.noreply.github.com>
2021-09-21 21:46:27 +0400

    object3

---
+++ pilevizator.php
@@ -0,0 +1,18 @@
+<?php
+
+class pulevizator {
+    public $flakon = false;
+    public $raspilitel = false;
+    public $spuskovoi_kr = false;
+
+    public function hranenie($flakon)
+    {
+        $flacon = true;
+    }
+
+    public function raspilenie($raspilitel, $spuskovoi_kr)
+    {
+        $raspilitel = true;
+        $spuskovoi_kr = true;
+    }
+}
